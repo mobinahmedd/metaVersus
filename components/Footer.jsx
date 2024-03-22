7d431be0bc956c4131d986eb2da7712ea7f7c587
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-03-23 00:03:02 +0500

    made year in footer dynamic

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,7 +19,10 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Enter the Metaverse
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
           <img
             src="/headset.svg"
             alt="headset"
@@ -35,11 +38,10 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            METAVERUS
-          </h4>
+          <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © {new Date().getFullYear()} Metaversus. All rights
+            reserved.
           </p>
 
           <div className="flex gap-4">
